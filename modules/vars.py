6f9db74f63577bf71d84eeb0459cf45dae55300b
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-14 23:55:04 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = "21008992"
 api_hash = "da87f6dea5ed8cfe1a53617e33a35742"
-bot_token = "7744027925:AAHjy9HEASeJWITns1IylGzn2abHInM3WBs"
+bot_token = "7685224488:AAF5AoylMyZQAZTNS3EiH0vyUcxZYgOLCpY"
 AUTH = "7548265642"
 #Bot Username = "MARCO_015"
