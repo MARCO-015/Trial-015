4a2811e0ae4b0512a521b77cb90bc164754d4522
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-16 09:56:33 +0530

    main.py

--- modules/main.py
+++ modules/main.py
@@ -151,13 +151,10 @@ async def account_login(bot: Client, m: Message):
             url = 'https://d1d34p8vz63oiq.cloudfront.net/380afa7e-c314-4d7a-8e5e-ca2167effd4a/master.mpd'
             if '/master.mpd' in url and not '/drm/' in url:
                 id = url.split("/")[-2]
-                api_url = f'https://pw-url-api-v1mf.onrender.com/process?v=https://sec1.pw.live/{id}/master.mpd&quality=raw_text2'
+                pwtoken = os.getenv('pwtoken')
+                api_url = f'https://madxapi-d0cbf6ac738c.herokuapp.com/{id}/master.m3u8?token={pwtoken}'
                 print(api_url)
 
-            elif '/master.mpd' in url:
-             id =  url.split("/")[-2]
-             url =  "https://d26g5bnklkwsh4.cloudfront.net/" + id + "/master.m3u8"
-              
             elif 'classplusapp' in url:
                   headers = {
                       'Host': 'api.classplusapp.com',
