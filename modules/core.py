de2d30c74d5ae512fbd0986395d9740b9e5b5c5f
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-16 17:17:43 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -181,7 +181,7 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
-    reply = await m.reply_text(f"**𝐔𝐩𝔩ØⒶ∂𝒾η𝑔** ⬆️ »\n`{name}`\n\n**【 𝒟𝑒𝓋𝑒𝓁𝑜𝓅𝑒𝒹 𝐵𝓎: 𝒜𝐼𝑀𝐸𝑅𝒮 ♡ 】**")
+    reply = await m.reply_text(f"**𝐔𝐩𝔩ØⒶ∂𝒾η𝑔** ⬆️ »\n`{name}`\n\n**【 Đєⓥⓔ𝓁ｏρєĐ 𝐁y: ΛＩＭΞＲＳ ♡ 】**")
     time.sleep(1)
     start_time = time.time()
     await m.reply_document(ka,caption=cc1)
@@ -195,12 +195,12 @@ async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
 async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
     await prog.delete (True)
-    reply = await m.reply_text(f"**𝐔𝐩𝔩ØⒶ∂𝒾η𝑔** ⬆️ »\n`{name}`\n\n**【 𝒟𝑒𝓋𝑒𝓁𝑜𝓅𝑒𝒹 𝐵𝓎: 𝒜𝐼𝑀𝐸𝑅𝒮 ♡ 】**")
+    reply = await m.reply_text(f"**𝐔𝐩𝔩ØⒶ∂𝒾η𝑔** ⬆️ »\n`{name}`\n\n**【 Đєⓥⓔ𝓁ｏρєĐ 𝐁y: ΛＩＭΞＲＳ ♡  】**")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
         else:
-            thumbnail = thumb
+            thumbnail = "thumb.jpg"
     except Exception as e:
         await m.reply_text(str(e))
 
